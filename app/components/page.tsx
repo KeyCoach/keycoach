38722011c2d2241827e8a7bd63b3dd96af7f1818
Christopher Kearl <c2_kearl@yahoo.com>
2025-01-18 12:47:26 -0700

    import color theme

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -4,10 +4,7 @@ import { ProgressBar } from "@/components/progress-bar";
 export default async function Components() {
   return (
     <div className="flex flex-col h-full">
-      <Divider> </Divider>
-      <Divider variant="sm"> </Divider>
-      <Divider variant="md"> </Divider>
-      <ProgressBar colorVariant="primary" />
+      
     </div>
   );
 }
