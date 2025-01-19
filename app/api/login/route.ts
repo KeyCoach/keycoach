6bfe5f7c9b650aaacfe5d0668e592556d94d91ba
corban <corbanpro@gmail.com>
2025-01-19 00:00:10 -0700

    validate password on login

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,4 +1,5 @@
 import { User } from "@/app/lib/types";
+import { BcryptVerifyPassword } from "@/service-interfaces/bcrypt";
 import { GetUserByEmail } from "@/service-interfaces/dynamo-db";
 import { CreateUserToken } from "@/service-interfaces/json-web-token";
 import { NextRequest } from "next/server";
@@ -23,16 +24,19 @@ export async function POST(request: NextRequest) {
 }
 
 /** Validate the login credentials. Return the user if valid */
-async function LoginValid(email: string, _password: string): Promise<User | null> {
+async function LoginValid(email: string, password: string): Promise<User | null> {
   const dbUser = await GetUserByEmail(email);
 
   if (!dbUser) {
     return null;
   }
 
-  // TODO: Validate password
+  if (!BcryptVerifyPassword(password, dbUser.passwordHash)) {
+    return null;
+  }
 
   return {
+    id: dbUser.id,
     fname: dbUser.fname,
     lname: dbUser.lname,
     email: dbUser.email,
