7dc8b46015a089bd8e50e9f300d6b750a4ff60b3
corban <corbanpro@gmail.com>
2025-01-21 20:28:29 -0700

    fix build errors

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -7,33 +7,30 @@ export default async function Components() {
       <div className="h-screen w-screen grid grid-cols-3 auto-rows-auto">
         <div id="centered-button" className="w-full flex flex-row flex-wrap items-start gap-4">
           <h1 className="text-3xl">Buttons</h1>
-          <Button
-            colorTheme="cerulean"
-            children={<span>cerulean button</span>}
-          />
-          <Button
-            colorTheme="obsidian"
-            children={<span>obsidian button</span>}
-          />
-          <Button
-            colorTheme="red"
-            children={<span>red button</span>}
-          />
-          <Button
-            colorTheme="amber"
-            children={<span>amber button</span>}
-          />
-          <Button
-            colorTheme="green"
-            children={<span>green button</span>}
-          />
-          <Button variant="previous-nav" children={<span className="ml-auto">previous</span>} />
-          <Button variant="next-nav" children={<span className="mr-auto">next</span>} />
+          <Button colorTheme="cerulean">
+            <span>cerulean button</span>
+          </Button>
+          <Button colorTheme="obsidian">
+            <span>obsidian button</span>
+          </Button>
+          <Button colorTheme="red">
+            <span>red button</span>
+          </Button>
+          <Button colorTheme="amber">
+            <span>amber button</span>
+          </Button>
+          <Button colorTheme="green">
+            <span>green button</span>
+          </Button>
+          <Button variant="previous-nav">
+            <span className="ml-auto">previous</span>
+          </Button>
+          <Button variant="next-nav">
+            <span className="mr-auto">next</span>
+          </Button>
         </div>
-        <div id="inputs" className="grid place-items-center">
-        </div>
-        <div id="modals" className="grid place-items-center">
-        </div>
+        <div id="inputs" className="grid place-items-center"></div>
+        <div id="modals" className="grid place-items-center"></div>
       </div>
 
       <div className="h-screen w-screen grid grid-cols-3 grid-rows-3">
