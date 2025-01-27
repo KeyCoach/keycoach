c1b146538c24756a6bac9069401542fd8342b0f6
Christopher Kearl <c2_kearl@yahoo.com>
2025-01-27 01:10:10 -0700

    adjust rootlayout padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
-        <div>
+        <div className="">
           <UserProvider user={user}>{children}</UserProvider>
         </div>
       </body>
