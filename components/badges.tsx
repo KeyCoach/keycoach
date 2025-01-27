d982fadf343dc2c24e7a671022b5852a23e64dbd
AngelinaWalton <95718621+AngelinaWalton@users.noreply.github.com>
2025-01-27 16:39:40 -0700

    Badge and Card Component

---
+++ components/badges.tsx
@@ -0,0 +1,32 @@
+import { Icon } from "@/components/icon";
+
+interface BadgeProps {
+  className?: string;
+  icon: string;
+  colorTheme?: "obsidian" | "cerulean" | "red" | "amber" | "green";
+  label?: string; // Add a prop for the text label
+}
+
+export function Badge({
+  className,
+  icon,
+  colorTheme = "cerulean",
+  label,
+}: BadgeProps) {
+  const colorClasses = {
+    obsidian: "bg-obsidian-500 text-slate-100",
+    cerulean: "bg-cerulean-500 text-slate-100",
+    red: "bg-red-500 text-slate-100",
+    amber: "bg-amber-500 text-slate-100",
+    green: "bg-green-500 text-slate-100",
+  };
+
+  const dynamicClass = `${colorClasses[colorTheme]} ${className} flex items-center gap-2 px-3 py-1 rounded shadow-md`;
+
+  return (
+    <div className={dynamicClass}>
+      {label && <span className="text-sm font-medium">{label}</span>}
+      <Icon src={`../icons/${icon}.svg`} alt={`${icon} icon`} w={18} h={18} />
+    </div>
+  );
+}
---
+++ components/card.tsx
@@ -0,0 +1,41 @@
+import { Badge } from "@/components/badges";
+import { Button } from "@/components/button";
+import { Icon } from "@/components/icon";
+
+
+interface CardProps {
+  title: string;
+  subtitle: string;
+  badgeIcon: string;
+  badgeTheme?: "obsidian" | "cerulean" | "red" | "amber" | "green";
+  buttonText: string;
+  onButtonClick?: () => void;
+}
+
+export function Card({
+  title,
+  subtitle,
+  badgeIcon,
+  badgeTheme = "green",
+  buttonText,
+}: CardProps) {
+  return (
+    <div className="w-full max-w-xs border rounded-lg shadow-md overflow-hidden transition-transform transform hover:scale-105 hover:shadow-lg">
+      <div className="p-3 bg-white-100">
+        <h3 className="text-lg font-bold">{title}</h3>
+        <p className="text-sm text-gray-600">{subtitle}</p>
+      </div>
+      <div className="flex items-center justify-between p-4 bg-slate-100">
+        <Badge icon={badgeIcon} colorTheme={badgeTheme} />
+        <Button
+          colorTheme="cerulean"
+          children={<span>{buttonText}</span>}
+        />
+      </div>
+      {/*Just an idea?
+      <div className="w-full bg-gray-100 rounded-full">
+        <div className="bg-green-500 h-2 rounded-full" style={{ width: "70%" }} />
+      </div>*/}
+    </div>
+  );
+}
