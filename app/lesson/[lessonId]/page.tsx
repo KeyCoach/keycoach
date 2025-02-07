a8bed1b86a48e5ce00cc53c3f39bfe5be0fa8603
corban <corbanpro@gmail.com>
2025-02-06 17:21:04 -0700

    fix typescript error

--- app/lesson/[lessonId]/page.tsx
+++ app/lesson/[lessonId]/page.tsx
@@ -1,7 +1,7 @@
 import { LessonFlow } from "./lesson-flow";
 
-export default async function LessonPage({ params }: { params: { lessonId: string } }) {
-  const { lessonId } = params;
+export default async function LessonPage({ params }: { params: Promise<{ lessonId: string }> }) {
+  const { lessonId } = await params;
 
   return (
     <div className="">
