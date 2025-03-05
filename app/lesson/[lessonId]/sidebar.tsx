506f8f2cf10f0fbcadc08524e46c96849135596e
corban <corbanpro@gmail.com>
2025-03-05 03:19:26 -0700

    use real data in side bars

--- app/lesson/[lessonId]/sidebar.tsx
+++ app/lesson/[lessonId]/sidebar.tsx
@@ -1,16 +1,11 @@
-// app/lesson/[lessonId]/sidebar.tsx
 import { useState } from "react";
+import { useLessonContext } from "./lesson-context";
 
-export default function Sidebar({
-  lessonId,
-  currentLevel,
-}: {
-  lessonId: string;
-  currentLevel: number;
-}) {
+export default function Sidebar() {
+  const { lessonPlan, currentStepIndex, lessonId } = useLessonContext();
   const [isCollapsed, setIsCollapsed] = useState(false);
 
-  const levels = Array.from({ length: 10 }, (_, i) => i + 1);
+  const levels = lessonPlan.steps;
 
   return (
     <aside
@@ -20,10 +15,7 @@ export default function Sidebar({
       id={`lesson-sidebar-${lessonId}`}
     >
       <div className="flex items-center justify-between rounded-t-lg p-4 dark:bg-slate-900">
-        <h2 className={`text-xl font-bold ${isCollapsed ? "hidden" : "block"}`}>
-          Lesson <span className="text-clip text-amber-700 dark:text-amber-200">{lessonId}</span>{" "}
-          Progress
-        </h2>
+        <h2 className={`text-xl font-bold ${isCollapsed ? "hidden" : "block"}`}>Lesson Progress</h2>
         <button
           className={`${isCollapsed ? "" : "ml-4"} flex h-8 w-8 items-center justify-center rounded-full bg-blue-500 text-slate-950 shadow-md hover:bg-blue-400 dark:text-slate-50`}
           onClick={() => setIsCollapsed(!isCollapsed)}
@@ -32,14 +24,19 @@ export default function Sidebar({
         </button>
       </div>
       <ul className="flex-1 overflow-y-auto">
-        {levels.map((level) => (
-          <li
-            key={level}
-            className={`${level < 10 ? "" : "rounded-lg"} p-4 ${level === currentLevel ? "bg-cerulean-600 font-bold text-slate-50 dark:bg-cerulean-300 dark:text-slate-950" : "hover:bg-gray-700 dark:bg-slate-900"} border border-slate-50 dark:border-slate-950`}
-          >
-            {isCollapsed ? level : `Level ${level}`}
-          </li>
-        ))}
+        {levels.map((level, i) => {
+          const isLastLevel = level === levels[levels.length - 1];
+          const isCurrentLevel = levels.indexOf(level) === currentStepIndex;
+          return (
+            <li
+              key={level.id}
+              className={`${isCollapsed && "text-center"} ${isLastLevel ? "rounded-lg" : ""} p-4 ${isCurrentLevel ? "bg-cerulean-600 font-bold text-slate-50 dark:bg-cerulean-300 dark:text-slate-950" : "hover:bg-gray-700 dark:bg-slate-900"} border border-slate-50 dark:border-slate-950`}
+            >
+              {i + 1}
+              {!isCollapsed && ` - ${level.name}`}
+            </li>
+          );
+        })}
       </ul>
     </aside>
   );
