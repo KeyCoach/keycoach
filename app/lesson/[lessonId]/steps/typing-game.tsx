70224701a09f447ab157c131bd4616fbad1cf486
Christopher Kearl <c2_kearl@yahoo.com>
2025-03-04 20:19:55 -0700

    type invader as it's own page

--- app/lesson/[lessonId]/steps/typing-game.tsx
+++ app/lesson/[lessonId]/steps/typing-game.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/button";
-import TypingAsteroids from "@/components/type-invader/TypingAsteroids";
+import TypeInvader from "@/components/type-invader/TypeInvader";
 
 export function TypingGame({ handleNextStep }: { handleNextStep: () => void }) {
   return (
     <>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <TypingAsteroids />
+      <div className="flex min-h-screen flex-col items-center justify-center gap-8">
+        <TypeInvader />
         <Button onClick={handleNextStep}>Next Step</Button>
       </div>
     </>
---
+++ app/type-invader/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+import TypeInvader from "@/components/type-invader/TypeInvader";
+import { Link } from "@/components/link";
+import Image from "next/image";
+
+export default function TypeInvaderPage() {
+  return (
+    <div className="w-text mx-auto mt-12">
+      <div className="pointer-events-none absolute inset-0">
+        {Array.from({ length: 30 }).map((_, i) => (
+          <div
+            key={i}
+            className="absolute rounded-full bg-white opacity-80 -z-10"
+            style={{
+              width: `${Math.random() * 4 + 2}px`,
+              height: `${Math.random() * 4 + 2}px`,
+              top: `${Math.random() * 100}%`,
+              left: `${Math.random() * 100}%`,
+            }}
+          ></div>
+        ))}
+      </div>
+
+      {/* type invader */}
+      <div className="overflow-hidden rounded-lg shadow">
+        <TypeInvader />
+      </div>
+
+      {/* skyline */}
+      <div className="absolute bottom-0 left-0 -z-10 flex w-full flex-col">
+        <Image
+          src="/img/skyline3.png"
+          alt="Skyline"
+          width={1920}
+          height={600}
+          className="block h-[412px] w-full object-cover"
+          priority
+        />
+        <div className="flex justify-center bg-black pb-4">
+          <Link href="/privacy" linkVariant="navbar-link">
+            <span className="text-slate-50">View our Privacy Policy</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
