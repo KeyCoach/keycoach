1cf9e652ad6315338bfc188df951149faed60e8c
Christopher Kearl <c2_kearl@yahoo.com>
2025-03-06 00:01:55 -0700

    new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import favicon from "@/public/whistle.png";
 import { Navbar } from "./navbar";
 import UserProvider from "./user-context";
 import { AuthenticateUser } from "@/utils/authenticate-user";
@@ -28,7 +29,10 @@ export default async function RootLayout({
       <head>
         <title>Keycoach</title>
         <meta name="description" content="Keycoach" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}
+        <link rel="icon" href={favicon.src} />
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon.src} />
+        <link rel="apple-touch-icon" href={favicon.src} />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} light:bg-slate-50 light:text-slate-950 flex max-h-screen min-h-screen flex-col dark:bg-slate-950 dark:text-slate-50`}
