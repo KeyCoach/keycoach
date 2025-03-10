1f816307784c9f711b87c4e9a46ae182d57f47a6
corban <corbanpro@gmail.com>
2025-03-09 22:00:43 -0600

    add wordlists

--- app/actions.ts
+++ app/actions.ts
@@ -2,9 +2,23 @@
 import { VerifyToken } from "@/service-interfaces/json-web-token";
 import { GetToken } from "@/utils/get-token";
 import { User } from "@/app/lib/types";
+import * as fs from "fs";
+import path from "path";
 
 export async function AuthenticateUser(): Promise<User | null> {
   const token = await GetToken();
   const user = VerifyToken(token);
   return user;
+}
+
+const filePath = path.join(process.cwd(), "wordlists/google-10000-english-usa-no-swears.txt");
+const wordList = fs.readFileSync(filePath, "utf-8");
+
+export async function getRandomWords(numLines: number): Promise<string[]> {
+  const lines = wordList.split("\n");
+  const randomLines = [];
+  for (let i = 0; i < numLines; i++) {
+    randomLines.push(lines[Math.floor(Math.random() * lines.length)]);
+  }
+  return randomLines;
 }
